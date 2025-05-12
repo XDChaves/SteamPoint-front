fed5509b782181d23434d595cedf4864918c115e
XDChaves <hpchavesxdr@gmail.com>
2025-05-12 19:36:25 -0300

    refactor: update HTML structure and enhance DataTable integration

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// O endereço da sua API Flask
+/* O endereço da sua API Flask
 const apiUrl = 'http://127.0.0.1:5000/';
+
+const apiUrl2 = 'http://127.0.0.1:5000/two';
 
 // Função assíncrona para buscar os dados
 async function buscarDadosDaApi() {
-    console.log('Tentando buscar dados da API...');
+    console.log('Tentando buscar dados da API1...');
 
     const response = await fetch(apiUrl);
     // Converte a resposta para JSON
     const dados = await response.json();
     // Faça algo com os dados recebidos
-    console.log('Dados recebidos da API:', dados);
-}
 
-// Adiciona um listener que chama a função quando o HTML inicial for carregado e parseado
-document.addEventListener('DOMContentLoaded', buscarDadosDaApi);
+    console.log('Dados recebidos da API1:', dados);
+}*/
